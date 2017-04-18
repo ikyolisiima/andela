f41937f3bc4b57568f200f55690bbb3ef77d4498
imma kyolisiima <imma kyolisiima>
2017-04-18 13:05:45 +0300

    third commit

--- prime_number_test.py
+++ prime_number_test.py
@@ -20,10 +20,13 @@ class prime_number_test(unittest.TestCase):
 from prime_number import prime_number
 class prime_number_test(unittest.TestCase):
 
-    def test_prime_number_one(self):
+   def test_prime_number_one(self):
         
         self.assertEqual(prime_number(2)[1], [2], msg="Result is invalid")
 
     
    def test_prime_number_two(self):        
         self.assertEqual(prime_number(3)[1], [2,3], msg="Result is invalid")
+       
+   def test_prime_number_three(self):        
+        self.assertEqual(prime_number(1)[1], [], msg="Result is invalid")
